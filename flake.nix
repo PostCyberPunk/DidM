b85ee70b4f24be83fedd08c744d50c8b5d915ded
PostCyberPunk <lyhguest2@outlook.com>
2025-06-05 19:54:24 +0800

    build: project init

---
+++ flake.nix
@@ -0,0 +1,83 @@
+{
+  description = "DidM Is a Dotfile Manager";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs @ {self, ...}: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux"];
+
+    forEachSupportedSystem = f:
+      inputs.nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          inherit system;
+          pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              inputs.rust-overlay.overlays.default
+              inputs.self.overlays.default
+            ];
+          };
+        });
+    mkPkg = pkgs:
+      pkgs.rustPlatform.buildRustPackage {
+        pname = "DidM";
+        version = "main";
+        src = ./.;
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+        };
+        # buildInputs = with pkgs; [
+        #   gtk3
+        #   gtk-layer-shell
+        # ];
+        nativeBuildInputs = with pkgs; [
+          cairo
+          pkg-config
+        ];
+      };
+  in {
+    overlays.default = final: prev: {
+      rustToolchain = let
+        rust = prev.rust-bin;
+      in
+        if builtins.pathExists ./rust-toolchain.toml
+        then rust.fromRustupToolchainFile ./rust-toolchain.toml
+        else if builtins.pathExists ./rust-toolchain
+        then rust.fromRustupToolchainFile ./rust-toolchain
+        else
+          rust.stable.latest.default.override {
+            extensions = ["rust-src" "rustfmt"];
+          };
+    };
+    packages = forEachSupportedSystem (args: {
+      DidM = mkPkg args.pkgs;
+      default = self.packages.${args.system}.DidM;
+    });
+
+    devShells = forEachSupportedSystem ({pkgs, ...}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          rustToolchain
+          openssl
+          pkg-config
+          cargo-deny
+          cargo-edit
+          cargo-watch
+          rust-analyzer
+          #############dev
+        ];
+
+        env = {
+          # Required by rust-analyzer
+          RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+        };
+      };
+    });
+  };
+}
