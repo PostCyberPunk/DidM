b8fa2469ecb03f4cbc1ec9663b8e9d80fa37c1b3
PostCyberPunk <lyhguest2@outlook.com>
2025-06-15 05:13:26 +0800

    fix check target

--- src/validation/check.rs
+++ src/validation/check.rs
@@ -5,11 +5,21 @@ pub fn check_git_repo(path: &Path) -> bool {
     path.join(".git").exists() || confirm("This is not a git repo, continue?")
 }
 pub fn check_target(path: &Path) -> bool {
-    !path.exists()
-        && confirm(&format!(
+    path.exists()
+        || confirm(&format!(
             "Target Path not exists: \n\
             {}\n\
             Do you want to create it?",
             path.display()
         ))
 }
+//NOTE: macro is not lazy
+// pub fn check_target(path: &Path) -> bool {
+//     let hint = &format!(
+//         "Target Path not exists: \n\
+//             {}\n\
+//             Do you want to create it?",
+//         path.display()
+//     );
+//     path.exists() || confirm(hint)
+// }
