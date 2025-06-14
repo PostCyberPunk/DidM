c46c3af586c03ac640a56ef4e0e4f75e81517f2e
PostCyberPunk <lyhguest2@outlook.com>
2025-06-15 04:34:40 +0800

    check target

--- src/validation/check.rs
+++ src/validation/check.rs
@@ -1,5 +1,15 @@
 use std::path::Path;
 
+use crate::cli::prompt::confirm;
 pub fn is_git_repo(path: &Path) -> bool {
     path.join(".git").exists()
+}
+pub fn check_target(path: &Path) -> bool {
+    !path.exists()
+        && confirm(&format!(
+            "Target Path not exists: \n\
+            {}\n\
+            Do you want to create it?",
+            path.display()
+        ))
 }
