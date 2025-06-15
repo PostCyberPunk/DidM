c9a3863d145ce4fd96661bd13acdda1b18ee959e
PostCyberPunk <lyhguest2@outlook.com>
2025-06-15 16:05:30 +0800

    fix prompt

--- src/cli/prompt.rs
+++ src/cli/prompt.rs
@@ -2,8 +2,7 @@
 //Ask user to confirm
 pub fn confirm(msg: &str) -> bool {
     let mut input = String::new();
-    println!("{}", msg);
-    print!("press y to confirm: ");
+    println!("{}\npress y to confirm:", msg);
     std::io::stdin().read_line(&mut input).unwrap();
     input.trim() == "y"
 }
