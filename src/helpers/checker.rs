884d6c68285a5763dac2a5632814e88460f823d1
PostCyberPunk <lyhguest2@outlook.com>
2025-06-16 03:35:28 +0800

    checker: return error

--- src/helpers/checker.rs
+++ src/helpers/checker.rs
@@ -1,4 +1,6 @@
+use anyhow::Result;
 use std::path::Path;
+use thiserror::Error;
 
 use crate::{cli::prompt::confirm, model::SkipCheck};
 
@@ -10,18 +12,41 @@ impl Checker {
     pub fn new(config: SkipCheck) -> Self {
         Checker { config }
     }
-    pub fn check_git_repo(&self, path: &Path) -> bool {
-        path.join(".git").exists() || confirm("This is not a git repo, continue?")
+    pub fn check_git_repo(&self, path: &Path) -> Result<()> {
+        if path.join(".git").exists()
+            || confirm(&format!(
+                "Current Path: {}\n\
+            This is not a git repo, continue?",
+                path.display()
+            ))
+        {
+            Ok(())
+        } else {
+            Err(CheckError::NotGitRepo.into())
+        }
     }
-    pub fn check_target(&self, path: &Path) -> bool {
-        path.exists()
+    pub fn check_target(&self, path: &Path) -> Result<()> {
+        if path.exists()
             || confirm(&format!(
                 "Target Path not exists: \n\
             {}\n\
             Do you want to create it?",
                 path.display()
             ))
+        {
+            Ok(())
+        } else {
+            Err(CheckError::TargetPathNotExists.into())
+        }
     }
+}
+
+#[derive(Error, Debug)]
+pub enum CheckError {
+    #[error("Not a git repo")]
+    NotGitRepo,
+    #[error("Target path not exists")]
+    TargetPathNotExists,
 }
 
 //NOTE: macro is not lazy
