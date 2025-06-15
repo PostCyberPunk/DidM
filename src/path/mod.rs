84af635988c0ae614ef4b4f1af7a9d028defb7a9
PostCyberPunk <lyhguest2@outlook.com>
2025-06-16 01:30:35 +0800

    refactor(path) error to mod

--- src/path/mod.rs
+++ src/path/mod.rs
@@ -1,33 +1,10 @@
+mod error;
 use anyhow::{Context, Result};
 use std::env;
 use std::fs;
 use std::path::{Path, PathBuf};
-use thiserror::Error;
 
-#[derive(Debug, Error)]
-pub enum PathError {
-    #[error("Environment variable `{0}` is missing")]
-    EnvVarMissing(String),
-
-    #[error("Failed to create parent directory: {0}")]
-    CreateDirFailed(String),
-
-    #[error("File {0} already existed in {1}")]
-    FileExists(String, String),
-
-    #[error("Permission denied: {0}")]
-    NoPermission(String),
-
-    #[error("Path is not a directory: {0}")]
-    NotDir(String),
-
-    #[error("Path is not a file: {0}")]
-    NotFile(String),
-
-    #[error("Failed to resolve path")]
-    ResolveFailed,
-}
-
+use error::PathError;
 //TODO: refactor this first
 //TODO: We have to remember to resolve the path before using it.
 //But, introduce a new struct that repsent the resolved path ,that does not feel right...
