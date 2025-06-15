3d3c07959870ef62611b5af7090d0d63f76b15df
PostCyberPunk <lyhguest2@outlook.com>
2025-06-16 03:08:00 +0800

    helper class init

--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -3,3 +3,18 @@ mod path;
 pub use checker::Checker;
 pub use path::PathError;
 pub use path::PathResolver;
+
+use crate::model::SkipCheck;
+
+pub struct Helpers {
+    pub checker: Checker,
+    pub path_resolver: PathResolver,
+}
+impl Helpers {
+    pub fn new(check_config: &SkipCheck) -> Self {
+        Helpers {
+            checker: Checker::new(*check_config),
+            path_resolver: PathResolver::new(check_config.unresolved_env),
+        }
+    }
+}
