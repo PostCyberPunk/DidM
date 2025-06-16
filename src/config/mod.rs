5ff542ff908ce5049b72875fa665e3521d4bf419
PostCyberPunk <lyhguest2@outlook.com>
2025-06-16 18:45:29 +0800

    loader:use congigset

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,58 +1,65 @@
 mod map;
+use crate::helpers::{PathResolver, ResolvedPath};
 use crate::model::DidmConfig;
-use crate::path::PathBufExtension;
-use anyhow::Result;
+use anyhow::{Context, Result};
 pub use map::ConfigMap;
 use std::fs;
-use std::path::PathBuf;
+
+//TODO: add detailed error handling for load config
 
 const CONFIG_FILE_NAME: &str = "didm.toml";
 const DEFAULT_PATH: &str = ".";
 const DEFAULT_CONFIG_PATH: &str = "./didm.toml";
-//TODO: add validation for config,ie. check duplicated profiles
-//
-//TODO: add detailed error handling for load config
 
-pub fn load_config(path: &str) -> Result<DidmConfig> {
-    let config_path = PathBuf::from(path).resolve()?;
-    if !config_path.exists() {
-        return Err(anyhow::anyhow!(
-            "Config file not found: {}",
-            config_path.display()
-        ));
-    }
-    let content = fs::read_to_string(&config_path)?;
-    let mut config: DidmConfig = toml::from_str(&content)?;
-    config.base_path = config_path.parent().unwrap().to_path_buf();
-    Ok(config)
+pub struct ConfigSet(ResolvedPath, DidmConfig);
+
+pub fn load_config(config_path: ResolvedPath) -> Result<ConfigSet> {
+    let content = fs::read_to_string(&config_path.get())?;
+    let config: DidmConfig = toml::from_str(&content)?;
+    Ok(ConfigSet(config_path, config))
 }
-pub fn load_configs(path: Option<&str>) -> Result<Vec<DidmConfig>> {
+pub fn load_configs(path: Option<&str>) -> Result<Vec<ConfigSet>> {
     let path = path.unwrap_or(DEFAULT_CONFIG_PATH);
-    if !PathBuf::from(path).exists() {
-        return Err(anyhow::anyhow!(
+    let resolver = &PathResolver::new(true);
+    //TODO: map this error to Hint
+
+    let resolved_config_path = resolver.resolve(path).with_context(|| {
+        format!(
             "Config file not found in current path,consider use `didm init` or specify path with `--path`"
-        ));
-    }
-    let base_config = load_config(path)?;
-    let mut result = base_config
+        )})?;
+    let base_path = resolved_config_path.to_parent().unwrap();
+
+    let base_configset = load_config(resolved_config_path)?;
+
+    let mut result: Vec<ConfigSet> = base_configset
+        .1
         .include
         .iter()
-        .map(|path| load_config(path))
-        .collect::<Result<Vec<DidmConfig>, _>>()?;
-    result.insert(0, base_config);
+        .map(|p| {
+            let _resolved_path = resolver.resolve_from(&base_path, p.as_str())?;
+            load_config(_resolved_path)
+        })
+        .collect::<Result<Vec<ConfigSet>, _>>()?;
+    result.insert(0, base_configset);
     Ok(result)
 }
 
 //TODO: Save multiple configs
-pub fn save_config(cfg: &DidmConfig) -> Result<()> {
-    let content = toml::to_string_pretty(cfg)?;
-    fs::write(&cfg.base_path, content)?;
+pub fn save_config(set: &ConfigSet) -> Result<()> {
+    let ConfigSet(config_path, config) = set;
+    let content = toml::to_string_pretty(config)?;
+    fs::write(&config_path.get(), content)?;
     Ok(())
 }
 
 pub fn init_config(path: Option<&str>) -> Result<()> {
     let path = path.unwrap_or(DEFAULT_PATH);
-    let config_path = PathBuf::from(path).find_file_or_ok(CONFIG_FILE_NAME)?;
-    let cfg = DidmConfig::new(config_path);
-    save_config(&cfg)
+    let resolver = PathResolver::new(true);
+
+    let resolved_path = resolver.resolve(path)?;
+
+    let config_path = resolved_path.into_child(CONFIG_FILE_NAME)?;
+    let config = DidmConfig::new();
+    let cfgset = ConfigSet(config_path, config);
+    save_config(&cfgset)
 }
