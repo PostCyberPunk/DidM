70cec77b44c59d5b14215d779de309b56d74349c
PostCyberPunk <lyhguest2@outlook.com>
2025-06-16 19:24:12 +0800

    loader:)also return base_path

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -18,7 +18,7 @@ pub fn load_config(config_path: ResolvedPath) -> Result<ConfigSet> {
     let config: DidmConfig = toml::from_str(&content)?;
     Ok(ConfigSet(config_path, config))
 }
-pub fn load_configs(path: Option<&str>) -> Result<Vec<ConfigSet>> {
+pub fn load_configs(path: Option<&str>) -> Result<(ResolvedPath, Vec<ConfigSet>)> {
     let path = path.unwrap_or(DEFAULT_CONFIG_PATH);
     let resolver = &PathResolver::new(true);
     //TODO: map this error to Hint
@@ -30,18 +30,15 @@ pub fn load_configs(path: Option<&str>) -> Result<Vec<ConfigSet>> {
     let base_path = resolved_config_path.to_parent().unwrap();
 
     let base_configset = load_config(resolved_config_path)?;
+    let mut config_sets = Vec::new();
 
-    let mut result: Vec<ConfigSet> = base_configset
-        .1
-        .include
-        .iter()
-        .map(|p| {
-            let _resolved_path = resolver.resolve_from(&base_path, p.as_str())?;
-            load_config(_resolved_path)
-        })
-        .collect::<Result<Vec<ConfigSet>, _>>()?;
-    result.insert(0, base_configset);
-    Ok(result)
+    for p in base_configset.1.include.iter() {
+        let _resolved_path = resolver.resolve_from(&base_path, p.as_str())?;
+        let s = load_config(_resolved_path)?;
+        config_sets.push(s);
+    }
+    config_sets.insert(0, base_configset);
+    Ok((base_path, config_sets))
 }
 
 //TODO: Save multiple configs
