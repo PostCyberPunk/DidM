f1925c678402531664519296b4ee155fc4f69da7
PostCyberPunk <lyhguest2@outlook.com>
2025-06-17 00:09:04 +0800

    plan_ctx:fit in

--- src/plan/ctx.rs
+++ src/plan/ctx.rs
@@ -1,10 +1,7 @@
-use std::path::PathBuf;
-
 use super::args::PlanArgs;
 use crate::config::ConfigMap;
-use crate::helpers::Helpers;
-use crate::model::{Behaviour, DidmConfig, Plan, Profile};
-use crate::path::PathBufExtension;
+use crate::helpers::{Helpers, ResolvedPath};
+use crate::model::{Behaviour, Plan, Profile};
 use crate::profile::{Backuper, ProfileContext};
 use crate::{
     commands::{CommandsContext, CommandsRunner},
@@ -13,15 +10,15 @@ use crate::{
 use anyhow::{Context, Result};
 
 pub struct PlanContext<'a> {
-    pub plan: &'a Plan,
-    pub name: &'a str,
-    pub commands_path: PathBuf,
-    pub profiles: Vec<(&'a Profile, usize, &'a str)>,
-    pub behaviour: Behaviour,
-    pub configs: &'a [DidmConfig],
     pub args: &'a PlanArgs,
     pub logger: &'a Logger,
+    pub base_path: &'a ResolvedPath,
+    pub plan: &'a Plan,
+    pub name: &'a str,
+    pub behaviour: Behaviour,
     pub helpers: &'a Helpers,
+    pub commands_path: ResolvedPath,
+    pub profiles: Vec<(&'a Profile, usize, &'a str)>,
 }
 
 impl<'a> PlanContext<'a> {
@@ -31,24 +28,34 @@ impl<'a> PlanContext<'a> {
         args: &'a PlanArgs,
         logger: &'a Logger,
     ) -> Result<Self> {
+        //NOTE: order should be: error with less calculation ; then error with lager calulation
         logger.info(&format!("Deploying plan : {} ...", plan_name));
-        let main_config = config_map.main_config;
-        let plan = config_map.get_plan(plan_name)?;
-        let profiles = config_map.get_profiles(&plan.profiles)?;
-        let behaviour = Behaviour::merge(&main_config.behaviour, &plan.override_behaviour);
 
-        let base_path = &main_config.base_path;
-        let commands_path = base_path.resolve_or_from(&plan.commands_path)?;
+        let base_path = config_map.get_main_base_path()?;
+        // (&plan.commands_path)?;
+        let plan = config_map.get_plan(plan_name)?;
+        let helpers = config_map.get_helpers();
+
+        let commands_path = helpers
+            .path_resolver
+            .resolve_from_or_base(base_path, &plan.commands_path)?;
+
+        let profiles = config_map.get_profiles(&plan.profiles)?;
+
+        let behaviour = config_map
+            .get_main_behaviour()
+            .override_by(&plan.override_behaviour);
+
         Ok(PlanContext {
+            args,
+            logger,
+            helpers,
+            base_path,
             plan,
             name: plan_name,
             profiles,
             commands_path,
             behaviour,
-            configs: config_map.configs,
-            args,
-            logger,
-            helpers: &config_map.helpers,
         })
     }
     pub fn deploy(&self) -> Result<()> {
@@ -57,15 +64,11 @@ impl<'a> PlanContext<'a> {
         let envrironment = &plan.environment;
         let args = self.args;
         let stop_at_commands_error = self.behaviour.stop_at_commands_error.unwrap();
-        let mut backuper = Backuper::init(
-            self.configs[0].base_path.clone(),
-            self.name.to_string(),
-            args.is_dry_run,
-        )?;
+        let mut backuper = Backuper::init(self.base_path, self.name.to_string(), args.is_dry_run)?;
         let cmds_runner = CommandsRunner::new(
             CommandsContext {
                 environment: envrironment,
-                path: &self.commands_path,
+                path: &self.commands_path.get(),
                 logger,
                 args,
                 stop_at_commands_error,
@@ -79,10 +82,7 @@ impl<'a> PlanContext<'a> {
         //FIX: initialize all profiles to a Vec,then apply
         for (profile, idx, profile_name) in self.profiles.iter() {
             logger.info(&format!("Applying profile: {}", profile_name));
-            let behaviour = match &profile.override_behaviour {
-                Some(b) => &self.behaviour.override_by(b),
-                None => &self.behaviour,
-            };
+            let behaviour = &self.behaviour.override_by(&profile.override_behaviour);
             let mut profile_ctx =
                 ProfileContext::new(profile_name, *idx, profile, self, behaviour, &mut backuper);
             profile_ctx
