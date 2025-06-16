5411e320f8ec63b057fa3866c7b0c2388e2711ef
PostCyberPunk <lyhguest2@outlook.com>
2025-06-16 21:36:03 +0800

    mainconfig:impl new

--- src/config/main_config.rs
+++ src/config/main_config.rs
@@ -1,3 +1,4 @@
+use crate::model::DidmConfig;
 use crate::model::SkipCheck;
 
 use crate::model::Behaviour;
@@ -5,4 +6,17 @@ use crate::model::Behaviour;
 pub struct MainConfig {
     pub(crate) behaviour: Behaviour,
     pub(crate) skipcheck: SkipCheck,
+}
+impl MainConfig {
+    pub fn new(config: &DidmConfig) -> Self {
+        let behaviour = Behaviour::new(&config.behaviour);
+        let skipcheck = match config.skip_check {
+            Some(c) => c,
+            None => SkipCheck::new(),
+        };
+        MainConfig {
+            behaviour,
+            skipcheck,
+        }
+    }
 }
