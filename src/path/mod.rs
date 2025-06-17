bd7144f6a6fe559aa6cacd61bd3633dd40d8a262
PostCyberPunk <lyhguest2@outlook.com>
2025-06-17 18:45:29 +0800

    use trait constrain

--- src/path/mod.rs
+++ src/path/mod.rs
@@ -8,23 +8,12 @@ pub use error::PathError;
 //TODO: We have to remember to resolve the path before using it.
 //But, introduce a new struct that repsent the resolved path ,that does not feel right...
 
-pub trait PathBufExtension: Sized {
-    fn to_string(&self) -> String
-    where
-        Self: AsRef<Path>,
-    {
+pub trait PathBufExtension: AsRef<Path> {
+    fn to_string(&self) -> String {
         self.as_ref().to_string_lossy().to_string()
     }
-    fn check_dir(&self) -> Result<()>;
-    fn check_permission(&self) -> Result<()>;
-
-    fn ensure_parent_exists(&self) -> Result<&Self>;
-    // fn find_file(&self, filename: &str) -> Result<Self>;
-}
-
-impl PathBufExtension for PathBuf {
     fn check_dir(&self) -> Result<()> {
-        if !self.is_dir() {
+        if !self.as_ref().is_dir() {
             return Err(PathError::NotDir(self.to_string()).into());
         }
         Ok(())
@@ -43,25 +32,27 @@ impl PathBufExtension for PathBuf {
         Ok(())
     }
 
-    // fn is_unresolved_absolute(&self) -> bool {
-    //     self.starts_with("$") || self.starts_with("~") || self.is_absolute()
-    // }
-    // fn resolved_from(self, base_path: &Path) -> Result<Self> {
-    //PERF: I decide to fart with my pants off
-    //but this time ,linter feels good about it
-    //------------------------------------
-    // if self.is_unresolved_absolute() {
-    //     return self.resolve();
-    // }
-    // let resolved = self.resolve()?;
-    // Ok(base_path.join(resolved))
-    // }
-
     fn ensure_parent_exists(&self) -> Result<&Self> {
-        if !self.exists() {
-            fs::create_dir_all(self.parent().unwrap())
+        if !self.as_ref().exists() {
+            fs::create_dir_all(self.as_ref().parent().unwrap())
                 .with_context(|| PathError::CreateDirFailed(self.to_string()))?;
         }
         Ok(self)
     }
+    // fn find_file(&self, filename: &str) -> Result<Self>;
 }
+
+impl PathBufExtension for PathBuf {}
+// fn is_unresolved_absolute(&self) -> bool {
+//     self.starts_with("$") || self.starts_with("~") || self.is_absolute()
+// }
+// fn resolved_from(self, base_path: &Path) -> Result<Self> {
+//PERF: I decide to fart with my pants off
+//but this time ,linter feels good about it
+//------------------------------------
+// if self.is_unresolved_absolute() {
+//     return self.resolve();
+// }
+// let resolved = self.resolve()?;
+// Ok(base_path.join(resolved))
+// }
