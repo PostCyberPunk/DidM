7787e73eb7bc420b019aac4b9066adf6ad447e43
PostCyberPunk <lyhguest2@outlook.com>
2025-06-17 21:39:36 +0800

    plan:...redo cmd runer

--- src/plan/ctx.rs
+++ src/plan/ctx.rs
@@ -18,7 +18,7 @@ pub struct PlanContext<'a> {
     pub name: &'a str,
     pub behaviour: Behaviour,
     pub helpers: &'a Helpers,
-    pub commands_path: ResolvedPath,
+    pub commands_runner: CommandsRunner<'a>,
     pub profiles: Vec<(&'a Profile, usize, &'a str)>,
 }
 
@@ -37,15 +37,29 @@ impl<'a> PlanContext<'a> {
         let plan = config_map.get_plan(plan_name)?;
         let helpers = config_map.get_helpers();
 
-        let commands_path = helpers
-            .path_resolver
-            .resolve_from_or_base(base_path, &plan.commands_path)?;
-
-        let profiles = config_map.get_profiles(&plan.profiles)?;
-
+        //Get Bhaviour
         let behaviour = config_map
             .get_main_behaviour()
             .override_by(&plan.override_behaviour);
+
+        //Prepare Command runner
+        let envrironment = &plan.environment;
+        let stop_at_commands_error = behaviour.stop_at_commands_error.unwrap();
+        let commands_path = helpers
+            .path_resolver
+            .resolve_from_or_base(base_path, &plan.commands_path)?;
+        let commands_runner = CommandsRunner::new(
+            CommandsContext {
+                environment: envrironment,
+                path: commands_path.get(),
+                logger,
+                args,
+                stop_at_commands_error,
+            },
+            &plan.pre_build_commands,
+            &plan.post_build_commands,
+        );
+        let profiles = config_map.get_profiles(&plan.profiles)?;
 
         Ok(PlanContext {
             args,
@@ -56,29 +70,14 @@ impl<'a> PlanContext<'a> {
             plan,
             name: plan_name,
             profiles,
-            commands_path,
+            commands_runner,
             behaviour,
         })
     }
     pub fn deploy(&self) -> Result<()> {
         let logger = self.logger;
-        let plan = self.plan;
-        let envrironment = &plan.environment;
-        let args = self.args;
-        let stop_at_commands_error = self.behaviour.stop_at_commands_error.unwrap();
         // let mut backuper = Backuper::init(self.base_path, self.name.to_string(), args.is_dry_run)?;
-        let cmds_runner = CommandsRunner::new(
-            CommandsContext {
-                environment: envrironment,
-                path: self.commands_path.get(),
-                logger,
-                args,
-                stop_at_commands_error,
-            },
-            &plan.pre_build_commands,
-            &plan.post_build_commands,
-        );
-        cmds_runner.run_pre_commands()?;
+        self.commands_runner.run_pre_commands()?;
 
         // Apply profiles
         for (profile, idx, profile_name) in self.profiles.iter() {
@@ -92,7 +91,7 @@ impl<'a> PlanContext<'a> {
                 .context(format!("Profile apply failed:{}", profile_name))?;
         }
 
-        cmds_runner.run_post_commands()?;
+        self.commands_runner.run_post_commands()?;
         Ok(())
     }
 }
