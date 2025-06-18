0d5fa36c84792b2191a434c723a38f018f939419
PostCyberPunk <lyhguest2@outlook.com>
2025-06-18 08:21:23 +0800

    entry pub

--- src/entries/entry.rs
+++ src/entries/entry.rs
@@ -9,10 +9,10 @@ pub enum BackupState {
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Entry {
-    source_path: PathBuf,
-    target_path: PathBuf,
-    overwrite_existed: bool,
-    bakcup_state: BackupState,
+    pub source_path: PathBuf,
+    pub target_path: PathBuf,
+    pub overwrite_existed: bool,
+    pub bakcup_state: BackupState,
 }
 
 impl Entry {
