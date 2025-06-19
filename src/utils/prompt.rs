d28eb066aa1ada13f9d76911335529997d5d8e5a
PostCyberPunk <lyhguest2@outlook.com>
2025-06-19 09:19:28 +0800

    fix confim on key press

--- src/utils/prompt.rs
+++ src/utils/prompt.rs
@@ -1,14 +1,24 @@
-use crossterm::event::{self, Event, KeyCode};
+use crossterm::{
+    event::{self, Event, KeyCode},
+    terminal::{disable_raw_mode, enable_raw_mode},
+};
 use std::io::{Write, stdout};
 //TODO: move this to a new mod so we can have both tui and cli
 //Ask user to confirm
 pub fn confirm(msg: &str) -> bool {
     println!("{}\npress y to confirm:", msg);
 
-    stdout().flush().unwrap();
-
-    if let Ok(Event::Key(key_event)) = event::read() {
-        return key_event.code == KeyCode::Char('y');
+    if stdout().flush().is_err() {
+        return false;
     }
-    false
+    if enable_raw_mode().is_err() {
+        return false;
+    }
+    let result = match event::read() {
+        Ok(Event::Key(key_event)) => key_event.code == KeyCode::Char('y'),
+        _ => false,
+    };
+
+    disable_raw_mode().unwrap();
+    result
 }
