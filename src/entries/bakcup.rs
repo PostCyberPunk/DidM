6dc4e8afb3d007511eb7bd15334ac4d37fc190ed
PostCyberPunk <lyhguest2@outlook.com>
2025-06-20 03:52:47 +0800

    backuk root

--- src/entries/bakcup.rs
+++ src/entries/bakcup.rs
@@ -11,6 +11,30 @@ use crate::{log::Logger, utils::PathExtension};
 
 //FIX: the ctx should be borrow from composition, not from sketch
 //initialize in sketch then it can be imutable
+pub struct BackupRoot {
+    base_dir: PathBuf,
+    is_dryrun: bool,
+}
+impl BackupRoot {
+    pub fn new(base_path: &ResolvedPath, comp_name: String, is_dryrun: bool) -> Result<Self> {
+        //Make sure we can write at the base path
+        base_path
+            .get()
+            .check_dir()
+            .and_then(|_| base_path.get().check_permission())
+            .with_context(|| BackupError::InitializeFailed)?;
+        //TODO: we can get data by meta data,we can have a better name
+        let now = Local::now().format("%Y_%m_%d_%H_%M_%S").to_string();
+        let base_dir = base_path
+            .get()
+            .join(".didm_backup")
+            .join(format!("composition_{}-{}", comp_name, now));
+        Ok(Self {
+            base_dir,
+            is_dryrun,
+        })
+    }
+}
 pub struct Backuper {
     ctx: Option<BackuperContext>,
     base_dir: PathBuf,
