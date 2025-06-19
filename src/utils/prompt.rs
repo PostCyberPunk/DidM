4a1844326b8f946d2bbe21c67402e15d5b150669
PostCyberPunk <lyhguest2@outlook.com>
2025-06-19 09:03:33 +0800

    feat(utils): pub confirm,confirm on key input

--- src/utils/prompt.rs
+++ src/utils/prompt.rs
@@ -1,8 +1,14 @@
+use crossterm::event::{self, Event, KeyCode};
+use std::io::{Write, stdout};
 //TODO: move this to a new mod so we can have both tui and cli
 //Ask user to confirm
 pub fn confirm(msg: &str) -> bool {
-    let mut input = String::new();
     println!("{}\npress y to confirm:", msg);
-    std::io::stdin().read_line(&mut input).unwrap();
-    input.trim() == "y"
+
+    stdout().flush().unwrap();
+
+    if let Ok(Event::Key(key_event)) = event::read() {
+        return key_event.code == KeyCode::Char('y');
+    }
+    false
 }
