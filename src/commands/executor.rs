5ffb34ccdbe70a4e8a62ecbe2577e03a9615e18c
PostCyberPunk <lyhguest2@outlook.com>
2025-06-19 22:29:34 +0800

    fis:encaps remove pub

--- src/commands/executor.rs
+++ src/commands/executor.rs
@@ -6,16 +6,27 @@ use std::{
 };
 
 pub struct CommandExecutor<'a> {
-    pub environment: &'a HashMap<String, String>,
-    pub path: &'a Path,
-    pub command: &'a String,
+    environment: &'a HashMap<String, String>,
+    path: &'a Path,
+    command: &'a String,
 }
 
 impl<'a> CommandExecutor<'a> {
+    pub fn new(
+        environment: &'a HashMap<String, String>,
+        path: &'a Path,
+        command: &'a String,
+    ) -> Self {
+        Self {
+            environment,
+            path,
+            command,
+        }
+    }
     pub fn run(&self) -> io::Result<Output> {
         Command::new("sh")
             .envs(self.environment)
-            .current_dir(&self.path)
+            .current_dir(self.path)
             .arg("-c")
             .arg(self.command)
             .output()
