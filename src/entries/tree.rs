e3d6e20eccdf4c6f0bb1a51ba7d052c0b04a4b13
PostCyberPunk <lyhguest2@outlook.com>
2025-07-01 06:16:43 +0800

    Revert "full tree"
    This reverts commit 38cfa75ae2f39c58e89674af3c4e1939bc7fc13c.

--- src/entries/tree.rs
+++ src/entries/tree.rs
@@ -11,7 +11,6 @@ enum EntryKind {
     Error(String),
 }
 
-#[derive(Debug)]
 struct TreeNode {
     children: BTreeMap<String, TreeNode>,
     kinds: HashSet<EntryKind>,
@@ -58,73 +57,49 @@ impl EntriesManager {
         }
 
         fn print_tree(node: &TreeNode, prefix: &str, last: bool, name: &str) {
-            if !name.is_empty() {
-                let branch = if last { "└── " } else { "├── " };
-                let label = label_list(&node.kinds);
-                if !label.is_empty() {
-                    println!("{}{}{} {}", prefix, branch, label, name);
+            //fold path
+            let mut curr_name = name.to_string();
+            let mut curr_node = node;
+            while curr_node.kinds.is_empty() && curr_node.children.len() == 1 {
+                let (child_name, child_node) = curr_node.children.iter().next().unwrap();
+                curr_name = if curr_name.is_empty() {
+                    child_name.clone()
                 } else {
-                    println!("{}{}{}", prefix, branch, name);
-                }
-            }
-            let len = node.children.len();
-            for (i, (child_name, child_node)) in node.children.iter().enumerate() {
-                let is_last = i == len - 1;
-                let mut path = child_name.clone();
-                let mut curr_node = child_node;
-                while curr_node.kinds.is_empty()
-                    && curr_node.children.len() == 1
-                    && curr_node.children.values().next().unwrap().kinds.is_empty()
-                    && curr_node.children.values().next().unwrap().children.len() == 1
-                {
-                    let (next_name, next_node) = curr_node.children.iter().next().unwrap();
-                    if path == "/" {
-                        path = String::new();
+                    if curr_name == "/" {
+                        curr_name = String::new();
                     }
-                    path = format!("{}/{}", path, next_name);
-                    curr_node = next_node;
-                }
-                if !curr_node.kinds.is_empty() && curr_node.children.is_empty() {
-                    let sub_label = label_list(&curr_node.kinds);
-                    if !sub_label.is_empty() {
-                        let new_prefix = if name.is_empty() {
-                            "".to_string()
-                        } else {
-                            format!("{}{}", prefix, if last { "    " } else { "│   " })
-                        };
-                        println!(
-                            "{}{}{} {}",
-                            new_prefix,
-                            if is_last { "└── " } else { "├── " },
-                            sub_label,
-                            path
-                        );
-                    }
-                } else {
-                    let new_prefix = if name.is_empty() {
-                        "".to_string()
-                    } else {
-                        format!("{}{}", prefix, if last { "    " } else { "│   " })
-                    };
-                    print_tree(curr_node, &new_prefix, is_last, &path);
-                }
+                    format!("{}/{}", curr_name, child_name)
+                };
+                curr_node = child_node;
             }
-        }
 
-        // 标签辅助函数
-        fn label_list(kinds: &HashSet<EntryKind>) -> String {
-            let mut label = String::new();
-            for kind in kinds {
-                match kind {
-                    EntryKind::Copy => label.push_str("[C]"),
-                    EntryKind::Link => label.push_str("[L]"),
-                    EntryKind::Skip => label.push_str("[S]"),
-                    EntryKind::Error(_) => label.push_str("[E]"),
+            //print child
+            if !curr_name.is_empty() {
+                let branch = if last { "└── " } else { "├── " };
+                let mut label = String::new();
+                for kind in &node.kinds {
+                    match kind {
+                        EntryKind::Copy => label.push_str("[C]"),
+                        EntryKind::Link => label.push_str("[L]"),
+                        EntryKind::Skip => label.push_str("[S]"),
+                        EntryKind::Error(e) => label.push_str("[E]"),
+                    }
                 }
+                print!("{}{}{} {}", prefix, branch, label, curr_name);
+                println!();
             }
-            label
+            let len = curr_node.children.len();
+
+            for (i, (child_name, child)) in curr_node.children.iter().enumerate() {
+                let is_last = i == len - 1;
+                let new_prefix = if curr_name.is_empty() {
+                    "".to_string()
+                } else {
+                    format!("{}{}", prefix, if last { "    " } else { "│   " })
+                };
+                print_tree(child, &new_prefix, is_last, child_name);
+            }
         }
-        // print!("{:#?}", &root);
         print_tree(&root, "", true, "");
     }
 }
